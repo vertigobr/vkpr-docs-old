e6348f80bfd716fffbc1fc319a3708c65260037a
Felipe Côrtes <contatos.cortes@gmail.com>
2020-10-30 10:04:56 -0300

    Clear gh-pages

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import { Redirect } from '@docusaurus/router';
-
-function Home() {
-  return <Redirect to={useBaseUrl('docs/')} />;
-}
-
-export default Home;
